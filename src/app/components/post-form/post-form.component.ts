caf81d5acb5d2b3b13c9717df3289466386fb913
Wren <grahamgrasley@gmail.com>
2024-09-10 16:40:17 -0400

    layout

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../../models/post';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-form.component.html',
   styleUrl: './post-form.component.css'
 })
@@ -14,11 +15,22 @@ export class PostFormComponent {
   @Output() submitted: EventEmitter<Post> = new EventEmitter<Post>();
 
   submitPost() : void {
+    if (!this.post.body && !this.post.title) {
+      this.cancel();
+      return;
+    }
+
     this.submitted.emit(this.post);
     this.post = {} as Post;
+    this.display = false;
   }
 
   toggleDisplay() : void {
     this.display = !this.display;
+  }
+
+  cancel() : void {
+    this.post = {} as Post;
+    this.display = false;
   }
 }
--- src/app/components/social-posts/social-posts.component.ts
+++ src/app/components/social-posts/social-posts.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Post } from '../../models/post';
+import { SinglePostComponent } from "../single-post/single-post.component";
+import { PostFormComponent } from "../post-form/post-form.component";
 
 @Component({
   selector: 'app-social-posts',
   standalone: true,
-  imports: [],
+  imports: [SinglePostComponent, PostFormComponent],
   templateUrl: './social-posts.component.html',
   styleUrl: './social-posts.component.css'
 })
